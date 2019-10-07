9b6bb3e82d69a4e664a79f6b4a8d741db7c88f5d
artas0000 <anisovich28@mail.ru>
2019-10-07 21:52:02 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
 module.exports = function zeros(expression) {
-  // your solution
+  var arrFactorials = expression.split('*');
+  var arrOneFactorial = [];
+  var arrTwoFactorial = [];
+
+  for (let i = 0; i < arrFactorials.length; i++) {
+    if (arrFactorials[i][arrFactorials[i].length - 2] == "!") {
+      arrTwoFactorial.push(parseInt(arrFactorials[i].slice(0, -2)));
+    } else {
+      arrOneFactorial.push(parseInt(arrFactorials[i].slice(0, -1)));
+    }
+  }
+
+  modFiveAndTwoInTwoFactorial = zeroInTwoFactorial(arrTwoFactorial);
+  modFiveAndTwoInOneFactorial = zeroInOneFactorial(arrOneFactorial);
+
+  allNumOfFive = modFiveAndTwoInTwoFactorial.numOfFive + modFiveAndTwoInOneFactorial.numOfFive;
+  allNumOfTwo = modFiveAndTwoInTwoFactorial.numOfTwo + modFiveAndTwoInOneFactorial.numOfTwo;
+
+  if (allNumOfTwo >= allNumOfFive) zero = allNumOfFive;
+  else zero = allNumOfTwo;
+
+  return zero
+
+  function zeroInOneFactorial(arr) {
+    let modFiveAndTwo = {
+      numOfTwo: 0,
+      numOfFive: 0
+    }
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 1; j <= arr[i]; j++) {
+        if (j % 25 == 0) modFiveAndTwo.numOfFive += 2;
+        else if (j % 5 == 0) modFiveAndTwo.numOfFive++;
+
+        if (j % 2 == 0) modFiveAndTwo.numOfTwo++;
+      }
+    }
+
+    return modFiveAndTwo
+  }
+
+  function zeroInTwoFactorial(arr) {
+
+    let modFiveAndTwo = {
+      numOfTwo: 0,
+      numOfFive: 0
+    }
+
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 == 1) {
+        for (let j = 1; j <= arr[i]; j += 2) {
+          if (j % 25 == 0) modFiveAndTwo.numOfFive += 2;
+          else if (j % 5 == 0) modFiveAndTwo.numOfFive++;
+        }
+      } else {
+        for (let j = 2; j <= arr[i]; j += 2) {
+          if (j % 25 == 0) modFiveAndTwo.numOfFive += 2;
+          else if (j % 5 == 0) modFiveAndTwo.numOfFive++;
+
+          if (j % 2 == 0) modFiveAndTwo.numOfTwo++;
+        }
+      }
+    }
+    return modFiveAndTwo
+  }
 }
